6f0a2087b8a62e1f1e903633a6abefc5c59a57fc
Pulak Bala <pulakbala0524@gmail.com>
2024-02-07 01:27:36 +0600

    create sidebar and navbar

--- app/ui/dashboard/sidebar/sidebar.js
+++ app/ui/dashboard/sidebar/sidebar.js
@@ -1,9 +1,112 @@
 import React from 'react'
+import styles from './sidebar.module.css'
+import Image from 'next/image'
+import {
+  MdDashboard,
+  MdSupervisedUserCircle,
+  MdShoppingBag,
+  MdAttachMoney,
+  MdWork,
+  MdAnalytics,
+  MdPeople,
+  MdOutlineSettings,
+  MdHelpCenter,
+  MdLogout,
+} from "react-icons/md";
+import MenuLink from './menuLink/menuLink';
 
 export default function Sidebar() {
+  //sidenavbar menu items 
+  const menuItems = [
+    {
+      title: "Pages",
+      list: [
+        {
+          title: "Dashboard",
+          path: "/dashboard",
+          icon: <MdDashboard />,
+        },
+        {
+          title: "Users",
+          path: "/dashboard/users",
+          icon: <MdSupervisedUserCircle />,
+        },
+        {
+          title: "Products",
+          path: "/dashboard/products",
+          icon: <MdShoppingBag />,
+        },
+        {
+          title: "Transactions",
+          path: "/dashboard/transactions",
+          icon: <MdAttachMoney />,
+        },
+      ],
+    },
+    {
+      title: "Analytics",
+      list: [
+        {
+          title: "Revenue",
+          path: "/dashboard/revenue",
+          icon: <MdWork />,
+        },
+        {
+          title: "Reports",
+          path: "/dashboard/reports",
+          icon: <MdAnalytics />,
+        },
+        {
+          title: "Teams",
+          path: "/dashboard/teams",
+          icon: <MdPeople />,
+        },
+      ],
+    },
+    {
+      title: "User",
+      list: [
+        {
+          title: "Settings",
+          path: "/dashboard/settings",
+          icon: <MdOutlineSettings />,
+        },
+        {
+          title: "Help",
+          path: "/dashboard/help",
+          icon: <MdHelpCenter />,
+        },
+      ],
+    },
+  ];
+  
   return (
-    <div>
-      <h1>Sidebar</h1>
+    <div className={styles.container}>
+      {/* user details  */}
+      <div className={styles.user}>
+        <Image className={styles.userImage} src="/noavatar.png" alt='' width="50" height="50"/>
+        <div className={styles.userDetail}>
+          <span className={styles.userName}>Pulak Bala</span>
+          <span className={styles.userTitle}>Bekar Boy</span>
+        </div>
+      </div>
+
+
+       <ul className={styles.list}>
+        {/* using map  */}
+        {menuItems.map((cat) => (
+          <li key={cat.title}>
+            <span className={styles.cat}>{cat.title}</span>
+            {cat.list.map((item) =>(
+              <MenuLink item={item} key={item.title}/>
+            ))}
+          </li>
+        ))}
+       </ul>
+       <button className={styles.logout}>
+          <MdLogout />
+          Logout
+        </button>
     </div>
   )
 }
